b2eeff62c9c35799d9aca1eeb19006300f05efbd
OFFTKP <parisoplop@gmail.com>
2021-12-21 01:01:24 +0200

    Create example_main.cpp

---
+++ example_main.cpp
@@ -0,0 +1,4 @@
+// Empty
+// TODO: create an example - gameboy.cpp contents -> gameboy_impl.cpp
+// gameboy should then inherit from emulator, gameboy_impl shouldn't
+// that way gameboy_impl can be used in other projects
